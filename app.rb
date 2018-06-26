f85af52d22a7fa7374bbba091c30c09533333f74
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-26 20:13:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,5 +8,12 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post '/food' do
+    params = {
+      :name => "Vaish"
+      :favorite_food => "Starberries and Cream"
+    }
+  post '/food' do
+      "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+
   end
 end
